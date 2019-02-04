c9e7c202b1b53f28a2413b0843c0695e614c2bb2
karawin <jp@cocatrix.fr>
2019-02-04 21:09:54 +0100

    update ucg_karadio32_fonts.h

--- src/clib/ucg_karadio32_fonts.h
+++ src/clib/ucg_karadio32_fonts.h
@@ -30,9 +30,14 @@ extern const ucg_fntpgm_uint8_t ucg_font_crox5tb[] UCG_FONT_SECTION("ucg_font_cr
 
 extern const ucg_fntpgm_uint8_t ucg_font_5x7_gr[] UCG_FONT_SECTION("ucg_font_5x7_gr");
 extern const ucg_fntpgm_uint8_t ucg_font_6x13_gr[] UCG_FONT_SECTION("ucg_font_6x13_gr");
+extern const ucg_fntpgm_uint8_t ucg_font_6x15_gr[] UCG_FONT_SECTION("ucg_font_6x15_gr");
 extern const ucg_fntpgm_uint8_t ucg_font_9x16_gr[] UCG_FONT_SECTION("ucg_font_9x16_gr");
+extern const ucg_fntpgm_uint8_t ucg_font_helvR12_gr[] UCG_FONT_SECTION("ucg_font_helvR12_gr");
 extern const ucg_fntpgm_uint8_t ucg_font_helvR14_gr[] UCG_FONT_SECTION("ucg_font_helvR14_gr");
 extern const ucg_fntpgm_uint8_t ucg_font_helvR18_gr[] UCG_FONT_SECTION("ucg_font_helvR18_gr");
+extern const ucg_fntpgm_uint8_t ucg_font_ncenR12_gr[] UCG_FONT_SECTION("ucg_font_ncenR12_gr");
 extern const ucg_fntpgm_uint8_t ucg_font_ncenR14_gr[] UCG_FONT_SECTION("ucg_font_ncenR14_gr");
+
+
 
 #endif /* _UCG_KARADIO32_H */
